e891f6cd9a8c9051f37db5a6c1e5f2ffda193c38
RomanovM <maxim.r@nopcommerce.com>
2025-03-30 22:20:08 +0200

    Recurring payments not supported yet

--- Nop.Plugin.Payments.PayPalCommerce/Components/Public/MessagesViewComponent.cs
+++ Nop.Plugin.Payments.PayPalCommerce/Components/Public/MessagesViewComponent.cs
@@ -61,8 +61,9 @@ namespace Nop.Plugin.Payments.PayPalCommerce.Components.Public
             var routeName = HttpContext.GetEndpoint()?.Metadata.GetMetadata<RouteNameMetadata>()?.RouteName;
             var isCartPage = routeName == PayPalCommerceDefaults.Route.ShoppingCart;
             var isPaymentMethodPage = routeName == PayPalCommerceDefaults.Route.PaymentInfo;
-            var isCheckoutPage = (HttpContext.Request.RouteValues?.TryGetValue("controller", out var controller) ?? false) &&
-                string.Equals(controller.ToString(), "Checkout", StringComparison.InvariantCultureIgnoreCase);
+            object controller = null;
+            var isCheckoutPage = (HttpContext.Request.RouteValues?.TryGetValue("controller", out controller) ?? false) &&
+                string.Equals(controller?.ToString() ?? string.Empty, "Checkout", StringComparison.InvariantCultureIgnoreCase);
             if (!isCartPage && !isPaymentMethodPage && !isCheckoutPage)
                 return Content(string.Empty);
 
--- Nop.Plugin.Payments.PayPalCommerce/Services/PayPalTokenService.cs
+++ Nop.Plugin.Payments.PayPalCommerce/Services/PayPalTokenService.cs
@@ -89,6 +89,10 @@ namespace Nop.Plugin.Payments.PayPalCommerce.Services
                 return;
             }
 
+            //don't insert tokens with no customer identifier
+            if (token.CustomerId == 0)
+                return;
+
             if (!tokens.Any())
                 token.IsPrimaryMethod = true;
 
