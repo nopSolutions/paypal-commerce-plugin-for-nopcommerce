9d7169bb12d662d7f5befe9c138158632b183f20
RomanovM <maxim.r@nopcommerce.com>
2025-04-18 20:53:07 +0200

    Use taxable shipping according to settings

--- Nop.Plugin.Payments.PayPalCommerce/Services/PayPalCommerceServiceManager.cs
+++ Nop.Plugin.Payments.PayPalCommerce/Services/PayPalCommerceServiceManager.cs
@@ -19,6 +19,7 @@ using Nop.Core.Domain.Orders;
 using Nop.Core.Domain.Payments;
 using Nop.Core.Domain.Shipping;
 using Nop.Core.Domain.Stores;
+using Nop.Core.Domain.Tax;
 using Nop.Core.Http.Extensions;
 using Nop.Plugin.Payments.PayPalCommerce.Domain;
 using Nop.Plugin.Payments.PayPalCommerce.Services.Api;
@@ -95,6 +96,7 @@ namespace Nop.Plugin.Payments.PayPalCommerce.Services
         private readonly PayPalCommerceHttpClient _httpClient;
         private readonly PayPalTokenService _tokenService;
         private readonly ShippingSettings _shippingSettings;
+        private readonly TaxSettings _taxSettings;
 
         #endregion
 
@@ -133,7 +135,8 @@ namespace Nop.Plugin.Payments.PayPalCommerce.Services
             PaymentSettings paymentSettings,
             PayPalCommerceHttpClient httpClient,
             PayPalTokenService tokenService,
-            ShippingSettings shippingSettings)
+            ShippingSettings shippingSettings,
+            TaxSettings taxSettings)
         {
             _currencySettings = currencySettings;
             _customerSettings = customerSettings;
@@ -169,6 +172,7 @@ namespace Nop.Plugin.Payments.PayPalCommerce.Services
             _httpClient = httpClient;
             _tokenService = tokenService;
             _shippingSettings = shippingSettings;
+            _taxSettings = taxSettings;
         }
 
         #endregion
@@ -466,7 +470,8 @@ namespace Nop.Plugin.Payments.PayPalCommerce.Services
             }
             var orderTotal = PrepareMoney(total.Value, details.CurrencyCode);
 
-            var shippingTotal = _orderTotalCalculationService.GetShoppingCartShippingTotal(details.Cart, includingTax: false);
+            var shippingTotal = _orderTotalCalculationService
+                .GetShoppingCartShippingTotal(details.Cart, includingTax: _taxSettings.ShippingPriceIncludesTax);
             var orderShippingTotal = PrepareMoney(shippingTotal ?? decimal.Zero, details.CurrencyCode);
 
             var taxTotal = _orderTotalCalculationService.GetTaxTotal(details.Cart, usePaymentMethodAdditionalFee: false);
